d8528550c4aa1bace1f936b3b297fc1cc81c503a
AhmedbGayess <38194881+AhmedbGayess@users.noreply.github.com>
2018-09-15 20:20:24 +0100

    fixing post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ import { ListGroupItem } from "react-bootstrap";
 
 const Post = ({ title, publishedAt, id }) => (
     <div className="postcard">
-        <Link to={`/edit/${id}`} className="post-link">
+        <Link to={`/link/${id}`} className="post-link">
             <ListGroupItem>
                 <h3 className="postcard__title">{title}</h3>
                 <p  className="postcard__text">{publishedAt}</p>
